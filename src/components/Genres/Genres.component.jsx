7befb4afeb3d41851433403f24b5325a9291aefb
fernando <nando4803@gmail.com>
2019-08-02 22:45:06 +0100

    Criando um novo component

--- src/components/Genres/Genres.component.jsx
+++ src/components/Genres/Genres.component.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Genres = () => (
-  <h1>Generos</h1>
-)
+const Genres = () =>{
+   const [data, setData] = useState([])
+   useEffect(() => {
+     axios
+        .get('/api/genres')
+        .then(res => {
+          setData(res.data.data)
+        })
+   }, [])
+
+   const renderizaLinha = record => {
+     return (
+       <tr key={record.id}>
+        <th scope="row">{record.id}</th>
+        <td>{record.name}</td>
+        <td></td>
+      </tr>
+     )
+   }
+
+   if(data.length === 0) {
+    return (
+      <div className="container">
+        <div className="alert alert-warning" role="alert">
+          Você não possui nenhum genêro!
+        </div>
+      </div>
+    )
+   }
+
+   return (
+     <div className="container">
+       <h1>Generos</h1>
+       <table className="table table-dark">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+            {data.map(renderizaLinha)}
+        </tbody>
+        </table>
+     </div>
+   )
+}
 
 export default Genres;
---
+++ src/components/NovoGenero/NovoGenero.component.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const NovoGenero = () => {
+  return (
+    <div className="container">
+      <h1>Novo Genêro</h1>
+    </div>
+  )
+}
+
+export default NovoGenero;
