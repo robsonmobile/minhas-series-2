a6bcdd54ac2970454629fd794a004d65b2039ba8
fernando <nando4803@gmail.com>
2019-08-02 14:13:30 +0100

    Adicionando routas na aplicação

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Header from './components/Header/Header.component';
+
+const Home = () => (
+  <h1>Home</h1>
+)
+
+const Genres = () => (
+  <h1>Generos</h1>
+)
+
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route path="/generos" component={Genres} />
+      </div>
+    </Router>
   );
 }
 
