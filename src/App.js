67386e458efb736811d6e021dccba1119cb79859
fernando <nando4803@gmail.com>
2019-08-02 15:14:37 +0100

    Corrigindo um erro

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
 
 import Header from './components/Header/Header.component';
 
@@ -12,12 +13,21 @@ const Genres = () => (
 )
 
 function App() {
+
+  const [data, setData] = useEffect({})
+  useEffect(() => {
+    axios.get('/api'.then(res => {
+      setData(res.data)
+    }))
+  }, [setData])
+
   return (
     <Router>
       <div>
         <Header />
         <Route exact path="/" component={Home} />
         <Route path="/generos" component={Genres} />
+        <pre>{JSON.stringify(data)}</pre>
       </div>
     </Router>
   );
